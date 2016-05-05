bbb81ed1d4486ca5e24e02bd80c5e475c9bb69ab
bberger_tgm <Benedikt@Benedikts-MacBook-Pro.local>
2016-05-05 18:12:17 +0200

    added comments, new insert and reading from couchbase

--- src/berger_mischek/CreateReadUpdateDelete.java
+++ src/berger_mischek/CreateReadUpdateDelete.java
@@ -16,14 +16,50 @@ public class CreateReadUpdateDelete {
 		Cluster cluster = CouchbaseCluster.create("192.168.74.135");
 		Bucket bucket = cluster.openBucket("default", 60, TimeUnit.SECONDS);
 		
+		// the JsonObject person1 will be created and filled with the necessary data
 		JsonObject person1 = JsonObject.empty()
 				.put("firstname", "Benedikt")
 				.put("lastname", "Berger")
 				.put("age", 18)
 				.put("city", "Wien");
 		
+		// creating json document with person1
 		JsonDocument doc1 = JsonDocument.create("bberger", person1);
-		JsonDocument response = bucket.upsert(doc1);
+		// insert the json document into the bucket 
+		JsonDocument response1 = bucket.upsert(doc1);
+		
+		// the JsonObject person1 will be created and filled with the necessary data
+		JsonObject person2 = JsonObject.empty()
+				.put("firstname", "Matthias")
+				.put("lastname", "Mischek")
+				.put("age", 18)
+				.put("city", "Korneuburg");
+		
+		// creating json document with person1
+		JsonDocument doc2 = JsonDocument.create("mmischek", person1);
+		// insert the json document into the bucket 
+		JsonDocument response2 = bucket.upsert(doc1);
+		
+		
+		JsonDocument bberger = bucket.get("bberger");
+		if (bberger == null) {
+			System.err.println("Document not found!");
+		} else {
+			// reading data
+			System.out.println("Last name: " + bberger.content().getString("lastname"));
+			System.out.println("First name: " + bberger.content().getString("firstname"));
+			System.out.println("Age: " + bberger.content().getInt("age"));
+			
+			// update bberger with a new age
+			bberger.content().put("Age", 18);
+			// update bucket
+			JsonDocument updated = bucket.replace(bberger);
+			
+			System.out.println("Age updated");
+			System.out.println("Age: " + bberger.content().getInt("age"));
+		}
+		
+		cluster.disconnect();
 		
 	}
 	
